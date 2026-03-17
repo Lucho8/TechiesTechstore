fa200bf00af0ce056afdb676e9aac3587ae64648
Lucho8 <lucianogonzalofredes@gmail.com>
2026-03-16 22:22:21 -0300

    changes for deploy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
@@ -25,7 +24,7 @@ app.get("/", (req, res) => {
   res.json({ message: "ITS ALIVE!" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
